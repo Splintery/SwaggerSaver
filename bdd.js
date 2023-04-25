1b520bb25e85bc26adc82b16c00ae8f60b81dd5d
ngy <ngyfrancois123@gmail.com>
2023-04-25 16:52:21 +0200

    'solution probleme body'

--- bdd.js
+++ bdd.js
@@ -1,20 +1,20 @@
 function bdd(){
 
     const pg = require('pg');
-    // const pool = new pg.Pool({
-    //     user: 'francois',
-    //     host: 'localhost',
-    //     database: 'bdd',
-    //     password: '123456',
-    //     port: 5432
-    // });
     const pool = new pg.Pool({
-        user: 'postgres',
+        user: 'francois',
         host: 'localhost',
         database: 'bdd',
-        password: '1234',
+        password: '123456',
         port: 5432
     });
+    // const pool = new pg.Pool({
+    //     user: 'postgres',
+    //     host: 'localhost',
+    //     database: 'bdd',
+    //     password: '1234',
+    //     port: 5432
+    // });
 
     let client;
 
