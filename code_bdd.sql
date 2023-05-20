f3177f6a8c2d69a3ad4ca643e29d280e84f01502
ngy <ngyfrancois123@gmail.com>
2023-05-20 15:34:48 +0200

    rajout de vetements dans la bdd + css panier

--- code_bdd.sql
+++ code_bdd.sql
@@ -56,6 +56,48 @@ INSERT INTO vetements (nom, chemin, prix, type_vetement, taille, stock) VALUES
 ('veste drill', '/img_vetements/veste_drill.jpeg', 34.99, 'Veste', 'S', 2),
 ('veste torrey', '/img_vetements/veste_torrey.webp', 19.99, 'Veste', 'L', 1);
 
-INSERT INTO panier (id_vetement) VALUES 
-(1),
-(4);
+-- Les joggings :
+INSERT INTO vetements (nom, chemin, prix, type_vetement, taille, stock) VALUES
+('Jogging TIE DYE', '/img_vetements/jogging1.jpeg', 34.99, 'Jogging', 'S', 123),
+('Jogging TIE DYE', '/img_vetements/jogging1.jpeg', 34.99, 'Jogging', 'L', 24),
+('Jogging bleu', '/img_vetements/jogging2.jpeg', 24.99, 'Jogging', 'XL', 9),
+('Jogging ComfyCush', '/img_vetements/jogging3.jpeg', 34.99, 'Jogging', 'S', 2),
+('Jogging Peace of Mind', '/img_vetements/jogging4.jpeg', 44.99, 'Jogging', 'S', 10),
+('Jogging Paisley Check', '/img_vetements/jogging5.jpeg', 34.99, 'Jogging', 'M', 71);
+
+-- Les accessoires :
+INSERT INTO vetements (nom, chemin, prix, type_vetement, taille, stock) VALUES
+('Bob', '/img_vetements/bob.jpeg', 14.99, 'Accessoire', 'S', 123),
+('Ceinture à carreau noir', '/img_vetements/ceinture1.webp', 12.99, 'Accessoire', 'S', 1),
+('Lunettes', '/img_vetements/lunettes1.webp', 34.99, 'Accessoire', 'S', 33),
+('Sac gris', '/img_vetements/sac1.webp', 25, 'Accessoire', 'S', 23),
+('Sac', '/img_vetements/sac2.webp', 49.99, 'Accessoire', 'S', 13);
+
+-- Les Tshirt :
+INSERT INTO vetements (nom, chemin, prix, type_vetement, taille, stock) VALUES
+('Tshirt gris', '/img_vetements/tshirt_gris.jpeg', 9.99, 'Tshirt', 'S', 42),
+('Tshirt gris', '/img_vetements/tshirt_gris.jpeg', 9.99, 'Tshirt', 'L', 23),
+('Tshirt gris', '/img_vetements/tshirt_gris.jpeg', 9.99, 'Tshirt', 'XL', 4),
+('Tshirt Haribo', '/img_vetements/tshirt_haribo.webp', 15.99, 'Tshirt', 'S', 20),
+('Tshirt noir', '/img_vetements/tshirt_noir.jpeg', 9.99, 'Tshirt', 'S', 422),
+('Tshirt noir', '/img_vetements/tshirt_noir.jpeg', 9.99, 'Tshirt', 'XL', 132),
+('Tshirt Oversize', '/img_vetements/tshirt_oversize.jpeg', 12.99, 'Tshirt', 'S', 28),
+('Tshirt Oversize', '/img_vetements/tshirt_oversize.jpeg', 12.99, 'Tshirt', 'S', 231),
+('Tshirt Peaces', '/img_vetements/tshirt_peaces.jpeg', 15.99, 'Tshirt', 'S', 27),
+('Tshirt Peaces', '/img_vetements/tshirt_peaces.jpeg', 15.99, 'Tshirt', 'XL', 9),
+('Tshirt rouge', '/img_vetements/tshirt_rouge.webp', 9.99, 'Tshirt', 'S', 4),
+('Tshirt Shroomscape', '/img_vetements/tshirt_shroomscape.webp', 20.99, 'Tshirt', 'M', 42),
+('Tshirt wash', '/img_vetements/tshirt_wash.jpeg', 14.99, 'Tshirt', 'S', 41),
+('Tshirt wash', '/img_vetements/tshirt_wash.jpeg', 14.99, 'Tshirt', 'XXL', 42);
+
+-- Les Jeans :
+INSERT INTO vetements (nom, chemin, prix, type_vetement, taille, stock) VALUES
+('Jeans ample', '/img_vetements/jeans_ample.webp', 29.99, 'Jeans', 'S', 123),
+('Jeans ample', '/img_vetements/jeans_ample.webp', 29.99, 'Jeans', 'XS', 90),
+('Jeans Denim', '/img_vetements/Jeans_denim.webp', 29.99, 'Jeans', 'S', 32);
+
+
+
+-- INSERT INTO panier (id_vetement) VALUES 
+-- (1),
+-- (4);
